f0bbb0ea3494246af7d371218055e0b0cbe5e8f5
Svetlana Yakovleva <lls2007@yandex.ru>
2022-12-27 15:48:11 +0300

    fix: create an architecture

--- main_service/src/main/java/ru/practicum/main_service/user/dao/UserStorage.java
+++ main_service/src/main/java/ru/practicum/main_service/user/dao/UserStorage.java
@@ -1,13 +1,9 @@
 package ru.practicum.main_service.user.dao;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.main_service.user.model.User;
 
-import java.util.List;
-
-
-public interface UserStorage extends JpaRepository<User, Long> {
-    List<User> findAllByIdIn(List<Long> ids, Pageable pageable);
+public interface UserStorage
+       // extends JpaRepository<User, Long>
+{
+    //List<User> findAllByIdIn(List<Long> ids, Pageable pageable);
 
 }
